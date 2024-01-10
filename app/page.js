b67163acd03e8cfcf121c09e67596812dc19b7fa
pranav123451 <pranavjadhav432@gmail.com>
2024-01-10 22:23:37 +0530

    video url upload

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <div className="homecontainer">
       <div className="videocontainer">
-      <video src={require('../public/TEDx.mp4')} autoPlay muted loop className='loader' />
+      <video src={require('https://res.cloudinary.com/dpa1bnqm3/video/upload/f_auto:video,q_auto/kdnajgnzmqic7ugnvus8')} autoPlay muted loop className='loader' />
       </div>
     </div>
   );
